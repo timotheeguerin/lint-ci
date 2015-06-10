6985b159e63d6c669966c6a2f65e0f039eacf354
Timothee Guerin <timothee.guerin@outlook.com>
2015-06-10 11:16:59 +0100

    Create link.jsx

---
+++ app/assets/javascripts/components/link.jsx
@@ -0,0 +1,27 @@
+var Link = React.createClass({
+    handleMouseDown: function (e) {
+        var node = $(e.target);
+        if (e.target.tagName !== 'A' && node.closest('a, .link')[0] === this.refs.link.getDOMNode()) {
+            e.preventDefault();
+        }
+    },
+    handleMouseUp: function (e) {
+        var node = $(e.target);
+        if (e.target.tagName !== 'A' && node.closest('a, .link')[0] === this.refs.link.getDOMNode()) {
+            if (e.button == 1) {
+                var win = window.open(this.props.href, '_blank');
+                win.focus();
+            } else if (e.button == 0) {
+                window.location = this.props.href;
+            }
+            e.preventDefault();
+        }
+    },
+    render: function () {
+        return (
+            <div ref='link' onMouseDown={this.handleMouseDown} onMouseUp={this.handleMouseUp} className={this.props.className + ' link'}>
+                            {this.props.children}
+            </div>
+        )
+    }
+});
