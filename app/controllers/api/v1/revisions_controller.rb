15da6162e27ac62f6390a3862b8627e4dd8c6d6e
Timothee Guerin <timothee.guerin@outlook.com>
2015-11-22 17:45:40 -0800

    fix #3

--- app/controllers/api/v1/revisions_controller.rb
+++ app/controllers/api/v1/revisions_controller.rb
@@ -6,14 +6,9 @@ class Api::V1::RevisionsController < Api::V1::BaseController
   # Github triggered hook
   def webhook
     @repository.transaction do
-      if @repository.refreshing
-        render json: {success: 'Repository already scanning!'}
-      else
-        job = ScanRepositoryJob.perform_later(@repository)
-        @repository.job_id = job.job_id
-        @repository.save
-        render json: {success: 'Repository queued for scan!'}, status: :accepted
-      end
+      queued = RevisionScan.new(@repository).scan
+      status = queued ? :accepted : :ok
+      render json: {success: 'Repository queued for scan!'}, status: status
     end
   end
 end
